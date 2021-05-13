a027bc445f78ddf6bd58a3b7026d97a67893ca5d
ronal <79209763+ronaljeider@users.noreply.github.com>
2021-05-12 22:20:19 -0500

    Add files via upload

---
+++ calculadora_edad.py
@@ -0,0 +1,10 @@
+año = input("¿en que año naciste?: ")
+año = int(año)
+año_a = input("¿cual es el año actual?: ")
+año_a = int(año_a)
+
+edad = año_a - año
+
+edad = str(edad)
+
+print("Tu edad es: " + edad + " años")
